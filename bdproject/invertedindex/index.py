77abd4fb9de45cabca49a254bf853d9148492750
otreblan <otreblain@gmail.com>
2021-06-27 08:10:04 -0500

    tweet2file

--- bdproject/invertedindex/index.py
+++ bdproject/invertedindex/index.py
@@ -1,5 +1,17 @@
 from typing import List
+from os.path import join
 
-def index_files(files: List[str]) -> None:
+file_prefix = "data"
+
+def tweet2file(id: int, text: str) -> str:
+    file_path = join(file_prefix, str(id))
+
+    with open(file_path, "w") as w:
+        w.write(text)
+
+    return file_path
+
+
+def index_json(files: List[str]) -> None:
     for file in files:
         print(file)
--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from bdproject.invertedindex.index import index_files
+from bdproject.invertedindex.index import index_json
 from sys import argv
 
 if __name__ == '__main__':
-    index_files(argv[1:])
+    index_json(argv[1:])
