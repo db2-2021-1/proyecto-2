d16ba42ac6a236f9052fd08c34afebc10228bc62
otreblan <otreblain@gmail.com>
2021-06-27 13:33:34 -0500

    Save to file

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+from pickle import dump
 from bdproject.invertedindex.index import index_json
 from sys import argv
 
 if __name__ == '__main__':
-    print(index_json(argv[1:]))
+    with open("index", "wb") as w:
+        dump(index_json(argv[1:]), w)
