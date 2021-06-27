69593050dfd30bdcb9e35669c70eb097d3b0a165
otreblan <otreblain@gmail.com>
2021-06-27 07:56:52 -0500

    Add index_files

--- bdproject/invertedindex/index.py
+++ bdproject/invertedindex/index.py
@@ -1,2 +1,5 @@
-def index() -> None:
-    print("a")
+from typing import List
+
+def index_files(files: List[str]) -> None:
+    for file in files:
+        print(file)
--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from bdproject.invertedindex.index import index
+from bdproject.invertedindex.index import index_files
+from sys import argv
 
 if __name__ == '__main__':
-    index()
+    index_files(argv[1:])
